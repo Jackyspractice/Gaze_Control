4c4b1b8a4aa0d18453f5ccb9e55380a40b4004a9
Jacky's Zenbook <s10828152@cycu.edu.tw>
2023-04-11 21:35:50 +0800

    version0

---
+++ gaze_detect.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import dlib
+from math import hypot  # X-Y Plant Distence
+import pyglet   # wav
+import time
+
---
+++ testQuestion.py
@@ -0,0 +1,12 @@
+from Ques_Window import *
+
+test = windows()
+
+test.Control("start")
+
+esc = input("Enter ESC to stop: ")
+
+if esc == "s":
+
+    test.control = "stop"
+
